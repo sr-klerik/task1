f263b32a4e2c8d81cadaac5fde6ca878c844b35a
Dmitry Makarov <d.makarov@nnov.starttelecom.ru>
2015-04-16 13:41:18 +0300

    Upgrade task1_2

--- task1_2.py
+++ task1_2.py
@@ -1,20 +1,49 @@
 #!/usr/bin/python
 
+import random
+
 def letters(big_string):
-    pass
+	result=""
+	if big_string.istitle():
+		for temp in big_string:
+			if 'A'<=temp<='Z':
+				result+=temp
+	return result
+
 
 def palindrome(pali):
-    pass
+	temp=0
+	while temp < len(pali)/2-1:
+		if pali[temp]==pali[-temp-1]:
+			temp+=1
+			result="True"
+		else: 
+			result="False" 
+			break
+	return result
+
 
 def find_letter(where, letter):
-    pass
+	result=[]
+	temp=where.split(" ")
+	for word in temp:
+		if word[0]==letter or word.lower()[0]==letter or word.upper()[0]==letter:
+			result.append(word)
+	return result
+
 
 def mix_words(just_string):
-    pass
+	result=""
+	temp=just_string.split(" ")
+	while temp:
+		i=random.randrange(len(temp))
+		result+=temp.pop(i)+" "
+	return result.capitalize()
+
 
 print letters("Trees Are So Kind")
 
-print palindrome("avid diva")
+print palindrome("avidsf fsdiva")
 
 print find_letter("Bears are the best animals ever", 'b')
 
