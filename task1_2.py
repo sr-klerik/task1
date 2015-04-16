9625f3548aa94ad2d420a60947394365ff08a96d
Dmitry Makarov <d.makarov@nnov.starttelecom.ru>
2015-04-16 13:40:33 +0300

    Upgrade task1_2

---
+++ task1_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+def letters(big_string):
+    pass
+
+def palindrome(pali):
+    pass
+
+def find_letter(where, letter):
+    pass
+
+def mix_words(just_string):
+    pass
+
+print letters("Trees Are So Kind")
+
+print palindrome("avid diva")
+
+print find_letter("Bears are the best animals ever", 'b')
+
+print mix_words("Bears are the best animals ever")
